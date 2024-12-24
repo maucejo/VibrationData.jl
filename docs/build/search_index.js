d4988f5deffc3eb26753b6d0f363f689e405e039
maucejo <79322171+maucejo@users.noreply.github.com>
2024-12-24 18:10:36 +0100

    update docs

--- docs/build/search_index.js
+++ docs/build/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VibrationData","category":"page"},{"location":"#VibrationData","page":"Home","title":"VibrationData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VibrationData.","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VibrationData","category":"page"},{"location":"#VibrationData.jl","page":"Home","title":"VibrationData.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VibrationData.","category":"page"}]
 }
